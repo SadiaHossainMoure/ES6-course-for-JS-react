a5a601899b2b49eeeb44717153db07babb94890f
Moure <sadiahossainmoure@gmail.com>
2024-02-11 11:43:22 +0600

    adddin

---
+++ temp.js
@@ -0,0 +1,22 @@
+// tenplate strings 
+
+
+// const firstName = 'Sadia';
+// const middleName = 'Hossain';
+// const lastName = 'Moure';
+
+// console.log(`My name is : ${firstName} ${middleName} ${lastName} `);
+
+
+// function getFullName(firstName, middleName, lastName) {
+//     return `${firstName} ${middleName} ${lastName}`;
+
+// }
+// const gGreeting = `${getFullName("sadia", "hossain", "moure")}`;
+// console.log (gGreeting);
+
+const getFullName=(firstName,middleName,lastName) =>{
+    return `${firstName} ${middleName} ${lastName}`;
+}
+
+console.log(`${getFullName("sadia", "hossain", "moure") }`);
