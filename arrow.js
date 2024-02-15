d175a6673a1c478e46141123b3f89e19f99a8937
Moure <sadiahossainmoure@gmail.com>
2024-02-16 03:08:58 +0600

    adddin

---
+++ arrow.js
@@ -0,0 +1,15 @@
+//function number() {
+//  return 10;
+//}
+//let number = () => 10;
+//console.log(number());
+
+//function number(n) {
+//  return 10;
+//}  
+
+//let number = (n) => 10;
+//console.log(number(10));
+
+//let number = (a,b) => a + b;
+//console.log(number(10, 5));
--- temp.js
+++ temp.js
@@ -20,3 +20,4 @@ const getFullName=(firstName,middleName,lastName) =>{
 }
 
 console.log(`${getFullName("sadia", "hossain", "moure") }`);
+
