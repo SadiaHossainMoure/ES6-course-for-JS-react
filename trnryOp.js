168f95001c138dc9eb026b24ea877319f831c6df
Moure <sadiahossainmoure@gmail.com>
2024-02-19 12:26:08 +0600

    adddin

---
+++ trnryOp.js
@@ -0,0 +1,5 @@
+var age = 9;
+
+var type = age >= 18 ? "adult" : age< 10 ? "child" : "young";
+    
+console.log(type);                         
---
+++ trnryOp2.js
@@ -0,0 +1,5 @@
+var isloggedin = true;
+
+var access = isloggedin ? true : false;
+
+console.log(access);
